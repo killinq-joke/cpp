bc9a83874a5cd3cc9a2dc2340acf5baf08cfb28a
Zakaria Touzri <ztouzri@c2r12p1.42nice.fr>
2021-06-30 18:30:24 +0200

    SWITCHED: stdio streams to putstr

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,27 +6,44 @@
 /*   By: ztouzri <ztouzri@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 17:21:19 by ztouzri           #+#    #+#             */
-/*   Updated: 2021/06/30 18:12:47 by ztouzri          ###   ########.fr       */
+/*   Updated: 2021/06/30 18:30:01 by ztouzri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include <unistd.h>
 
-int	main(int ac, char **av)
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
 {
 	int	i;
 
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int		i;
+	char	str[] = "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+
 	if (ac < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		ft_putstr(str);
 	else
 	{
 		i = 1;
 		while (i < ac)
 		{
-			std::cout << av[i] << " ";
+			ft_putstr(av[i]);
 			i++;
 		}
-		std::cout << std::endl;
 	}
 	return (0);
 }
