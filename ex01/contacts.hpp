25b7e09f1b48bc2b4611a762b4adf1f542f0f44b
Zakaria Touzri <ztouzri@c1r2p6.42nice.fr>
2021-07-05 10:15:07 +0200

    ADDED: EXIT

--- ex01/contacts.hpp
+++ ex01/contacts.hpp
@@ -1,35 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   contacts.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ztouzri <ztouzri@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/01 11:09:32 by ztouzri           #+#    #+#             */
-/*   Updated: 2021/07/05 10:10:10 by ztouzri          ###   ########.fr       */
+/*   Created: 2021/07/02 19:58:11 by ztouzri           #+#    #+#             */
+/*   Updated: 2021/07/05 10:11:22 by ztouzri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string>
-#include <iostream>
-#include "contacts.hpp"
+#ifndef CONTACTS_HPP
+# define CONTACTS_HPP
 
-int	main(int ac, char **av)
-{
-	ac = 0;
-	av = 0;
-	std::string	name;
-	while (name != "EXIT")
-	{
-		std::cin >> name;
-		if (name == "ADD")
-		{
+class Contact {
+	
+};
 
-		}
-		else if (name == "SEARCH")
-		{
-
-		}
-	}
-	return (0);
-}
+#endif
