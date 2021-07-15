c2f8cc43f0049ae1bc6578dd78185a7bc425dbdf
Zakaria Touzri <ztouzri@c1r4p8.42nice.fr>
2021-07-15 21:41:05 +0200

    ADDED: plasmarifle

--- c04/ex01/PlasmaRifle.cpp
+++ c04/ex01/PlasmaRifle.cpp
@@ -6,7 +6,21 @@
 /*   By: ztouzri <ztouzri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 20:06:40 by ztouzri           #+#    #+#             */
-/*   Updated: 2021/07/15 20:06:41 by ztouzri          ###   ########.fr       */
+/*   Updated: 2021/07/15 21:39:41 by ztouzri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PlasmaRifle.hpp"
+
+PlasmaRifle::PlasmaRifle(void) : AWeapon("Plasma Rifle", 5, 21)
+{
+}
+
+PlasmaRifle::~PlasmaRifle(void)
+{
+}
+
+void	PlasmaRifle::attack(void) const
+{
+	cout << "* piouuu piouuu piouuu *" << endl;
+}
--- c04/ex01/PlasmaRifle.hpp
+++ c04/ex01/PlasmaRifle.hpp
@@ -6,7 +6,22 @@
 /*   By: ztouzri <ztouzri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 20:06:42 by ztouzri           #+#    #+#             */
-/*   Updated: 2021/07/15 20:06:43 by ztouzri          ###   ########.fr       */
+/*   Updated: 2021/07/15 21:40:55 by ztouzri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PLASMARIFLE_HPP
+# define PLASMARIFLE_HPP
+# include "AWeapon.hpp"
+
+class	PlasmaRifle : public AWeapon
+{
+
+public:
+	PlasmaRifle(void);
+	~PlasmaRifle(void);
+	
+	virtual void	PlasmaRifle::attack(void) const;
+};
+
+#endif
